a0326b498bc0fe2ec3514bec0e117b6d78c09f58
ayork22 <agyork22@gmail.com>
2016-06-03 13:05:41 -0600

    Added CLW Command

--- DroppedAgent/src/DroppedAgent.java
+++ DroppedAgent/src/DroppedAgent.java
@@ -1,9 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class DroppedAgent {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
+		    try {
+		      String line;
+		      Process p = Runtime.getRuntime().exec("java -Duser=admin -Dhost=192.168.99.100 -jar /Users/yoral01/DevStuff/CLWorkstation.jar get cluster configuration");
+		      BufferedReader input =
+		        new BufferedReader
+		          (new InputStreamReader(p.getInputStream()));
+		      while ((line = input.readLine()) != null) {
+		        System.out.println(line);
+		      }
+		      input.close();
+		    }
+		    catch (Exception err) {
+		      err.printStackTrace();
+		    }
+		  }
 	}
-
-}
