f44b9096db43f5d380c95ced1ffb604f93ae23fa
ayork22 <agyork22@gmail.com>
2016-06-05 09:48:24 -0600

    Added Emailer & initial properties

--- DroppedAgent/src/DroppedAgent.java
+++ DroppedAgent/src/DroppedAgent.java
@@ -7,46 +7,71 @@ import java.io.InputStreamReader;
 public class DroppedAgent {
 
 	public static void main(String[] args) {
+		
+		String line;
+		String clwLocation = "/Users/yoral01/DevStuff/CLWorkstation.jar";
+		String emHost = "192.168.99.100";
+		String emPort;
+		String user = "admin";
+		String pass;
+		String clwCommand = "list agents matching .*";
+		String javaLocation = "java";
+		String Agent = "WAS Liberty Docker1";
+		
 		try {
+			// Template for using File
+			// File file = new File("/tmp/AgentList.txt");
+			// // if file doesn't exists, then create it
+			// if (!file.exists()) {
+			// file.createNewFile();
+			// }
 
-			File file = new File("/tmp/AgentList.txt");
-			// if file doesnt exists, then create it
-			if (!file.exists()) {
-				file.createNewFile();
-			}
+			
+//			Process p = Runtime.getRuntime().exec(
+//					"java -Duser=admin -Dhost=192.168.99.100 -jar /Users/yoral01/DevStuff/CLWorkstation.jar list agents matching .*");
 
-			String line;
-			Process p = Runtime.getRuntime().exec(
-					"java -Duser=admin -Dhost=192.168.99.100 -jar /Users/yoral01/DevStuff/CLWorkstation.jar list agents matching .*");
-
+			Process p = Runtime.getRuntime().exec(javaLocation + " -Duser=" + user + " -Dhost=" + emHost + " -jar " + clwLocation + " " + clwCommand);
+			
+			String test = javaLocation + " -Duser=" + user + " -Dhost=" + emHost + " -jar " + clwLocation + " " + clwCommand;
+			System.out.println("test =" + test);
 			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-			// Create String Builder
+			// Create String Builder to handle in memory
 			StringBuilder sb = new StringBuilder();
 
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
+			// Use FileWriter if want to write CLW output to file
+			// FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			// BufferedWriter bw = new BufferedWriter(fw);
+
 			while ((line = input.readLine()) != null) {
-				// System.out.println(line);
-				bw.append(line);
-				bw.newLine();
-				bw.append("test");
+
+				// Used for writing to File
+				// bw.append(line);
+				// bw.newLine();
+				// bw.append("test");
 
 				// Build String in WHILE loop
 
 				sb.append(line);
-				sb.append(System.getProperty("line.separator"));
+				// Adds line if desired
+				// sb.append(System.getProperty("line.separator"));
 
 			}
 			// Print FINAL String of all Agents
 			String result = sb.toString();
 			System.out.println("Agent List = " + result);
 			// Close connections
-			bw.close();
+			// bw.close();
 			input.close();
-			System.out.println("File Written to");
-			
-			System.out.println("Find Docker = " + result.contains("DockerContainer"));
+
+			// System.out.println("Find Docker = " + result.contains("WAS
+			// Liberty Docker"));
+			if (result.contains(Agent)) {
+				System.out.println("Agent is Connected");
+			} else {
+				System.out.println("Agent is DisConnected");
+				SendEmail.sendemail();
+			}
 		} catch (Exception err) {
 			err.printStackTrace();
 		}
